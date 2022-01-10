3c63d4e41ff324cee187e049b0bd074a8dac54d7
Ryan Soeyadi <rsoeyadi@me.com>
2022-01-10 14:30:11 -0500

    Removed cache

--- scrape.py
+++ scrape.py
@@ -3,18 +3,20 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 from selenium.webdriver.firefox.options import Options
+from certificate import certificate
 
 class Event:
     def __init__(self, performer, month, day, time, location, instrumentation, link):
         self.performer = performer
         self.month = month
         self.day = day
+        self.time = time 
         self.location = location
         self.instrumentation = instrumentation
         self.link = link
-        
+
 options = Options()
-options.headless = True
+options.headless = False
 driver = webdriver.Firefox(options=options)
 driver.get("https://www.juilliard.edu/stage-beyond/performance/calendar")
 
@@ -41,8 +43,8 @@ for eventGroupUL in eventGroupsULs:
         
         for eventGroup in eventGroups:
 
-                month = eventGroup.find_element_by_class_name("month")
-                day = eventGroup.find_element_by_class_name("day")
+                month = eventGroup.find_element_by_class_name("month").text
+                day = eventGroup.find_element_by_class_name("day").text
                 
                 event_group_events = eventGroup.find_element_by_class_name("event-group-events")
                 events = event_group_events.find_elements_by_class_name("event")
@@ -63,13 +65,42 @@ for eventGroupUL in eventGroupsULs:
                             instrumentation = None 
                     link = article.find_element_by_tag_name("a").get_attribute("href")
                     
-                    location = article.find_element_by_class_name("field--name-field-venue")
-                    time = article.find_element_by_tag_name("time")
+                    location = article.find_element_by_class_name("field--name-field-venue").text
+                    time = article.find_element_by_tag_name("time").text
                     
                     new_event = Event(performer, month, day, time, location, instrumentation, link)
                     eventsData.append(new_event)
 
-                     
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import datetime
+from datetime import datetime
+import pytz
+
+tz = pytz.timezone('America/New_York')
+time = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+
+cred = credentials.Certificate(certificate)
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+eventsJSON = {}
+#convert to JSON
+for event in eventsData:
+        eventsJSON[event.performer] = {}
+        eventsJSON[event.performer]['instrumentation'] = event.instrumentation
+        eventsJSON[event.performer]['location'] = event.location
+        eventsJSON[event.performer]['month'] = event.month
+        eventsJSON[event.performer]['day'] = event.day
+        eventsJSON[event.performer]['time'] = event.time
+        eventsJSON[event.performer]['link'] = event.link
+
+def save(collection_id, document_id, data):
+    db.collection(collection_id).document(document_id).set(data)
+
+save(collection_id = "Events", document_id = f"{time}", data=eventsJSON)
 
 
 
